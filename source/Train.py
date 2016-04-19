f9a75d8bed60053b238efdd826ef82eaa1234b30
Divam Gupta <divamgupta@gmail.com>
2016-04-19 09:11:50 +0530

    Trainer completed ... reverted to keras 0.3.3

--- source/Train.py
+++ source/Train.py
@@ -6,7 +6,7 @@ import os
 import os.path
 import argparse
 import json
-
+import math
 
 import Model
 import Vectorize
@@ -15,7 +15,7 @@ import Vectorize
 parser = argparse.ArgumentParser()
 parser.add_argument("--exp_name", type = str , default = "main")
 parser.add_argument("--data", type = str)
-parser.add_argument("--batch_size", type = int, default = 128)
+parser.add_argument("--batch_size", type = int, default = 64 )
 parser.add_argument("--epochs", type = int, default = 5 )
 args = parser.parse_args()
 
@@ -36,7 +36,7 @@ TOTAL_EPOCHS = args.epochs
 BATCH_SIZE = args.batch_size
 EXP_NAME  = args.exp_name
 
-totalBatches =  int(len(paths) / BATCH_SIZE)
+totalBatches =  math.ceil( (len(paths)+0.0) / BATCH_SIZE)
 
 batchesDone = 0 
 epochsDone = 0
@@ -45,26 +45,26 @@ epochsDone = 0
 
 def saveState():
 
-	global TOTAL_EPOCHS , BATCH_SIZE , EXP_NAME , batchesDone , epochsDone
+	global TOTAL_EPOCHS , BATCH_SIZE , EXP_NAME , batchesDone , epochsDone , discriminator_on_generator , generator ,  discriminator
 
-	with open('../data/weights/'+EXP_NAME+'varstate', 'w') as outfile:
+	with open('../data/weights/'+EXP_NAME+'-varstate', 'w') as outfile:
 		d = {  "TOTAL_EPOCHS":TOTAL_EPOCHS , "BATCH_SIZE":BATCH_SIZE , "EXP_NAME":EXP_NAME , "batchesDone":batchesDone , "epochsDone":epochsDone , "totalBatches":totalBatches }
 		json.dump( d  , outfile)
 
-	generator.save_weights('../data/weights/'+EXP_NAME+'generator', True)
-	discriminator.save_weights('../data/weights/'+EXP_NAME+'discriminator', True)
+	generator.save_weights('../data/weights/'+EXP_NAME+'-generator', True)
+	discriminator.save_weights('../data/weights/'+EXP_NAME+'-discriminator', True)
 
 
 def isSavedState():
 	global EXP_NAME
-	return os.path.isfile('../data/weights/'+EXP_NAME+'varstate') 
+	return os.path.isfile('../data/weights/'+EXP_NAME+'-varstate') 
 
 
 def loadState():
 	
-	global TOTAL_EPOCHS , BATCH_SIZE , EXP_NAME , batchesDone , epochsDone
+	global TOTAL_EPOCHS , BATCH_SIZE , EXP_NAME , batchesDone , epochsDone , discriminator_on_generator , generator ,  discriminator
 
-	with open('../data/weights/'+EXP_NAME+'varstate') as outfile:
+	with open('../data/weights/'+EXP_NAME+'-varstate') as outfile:
 		d = json.load(  outfile)
 
 	TOTAL_EPOCHS = d['TOTAL_EPOCHS']
@@ -74,28 +74,29 @@ def loadState():
 	batchesDone = d['batchesDone']
 	epochsDone = d['epochsDone']
 
-	generator.load_weights('../data/weights/'+EXP_NAME+'generator', True)
-	discriminator.load_weights('../data/weights/'+EXP_NAME+'discriminator', True)
+	generator.load_weights('../data/weights/'+EXP_NAME+'-generator')
+	discriminator.load_weights('../data/weights/'+EXP_NAME+'-discriminator')
 
 
 
 
 def trainOnBatch(batchNo):
-	global TOTAL_EPOCHS , BATCH_SIZE , EXP_NAME , batchesDone , epochsDone , paths
+	global TOTAL_EPOCHS , BATCH_SIZE , EXP_NAME , batchesDone , epochsDone , paths , discriminator_on_generator , generator ,  discriminator
 
 	batch = paths[batchNo*BATCH_SIZE : (batchNo+1)*BATCH_SIZE]
+	batchSize = len(batch)
 	image_batch = Vectorize.getImages(batch)
-	noise = Vectorize.getRandomNoise(BATCH_SIZE)
+	noise = Vectorize.getRandomNoise(batchSize)
 	generated_images = generator.predict(noise)
 
 	X = np.concatenate((image_batch, generated_images))
-	y = [1] * BATCH_SIZE + [0] * BATCH_SIZE
+	y = [1] * batchSize + [0] * batchSize
 
-	noise = Vectorize.getRandomNoise(BATCH_SIZE)
+	noise = Vectorize.getRandomNoise(batchSize)
 
 	d_loss = discriminator.train_on_batch(X, y)
-	g_loss = discriminator_on_generator.train_on_batch(noise, [1] * BATCH_SIZE)
-
+	g_loss = discriminator_on_generator.train_on_batch(noise, [1]*batchSize )
+	print "lodsss" , d_loss , g_loss
 	return d_loss , g_loss
 
 
@@ -104,16 +105,22 @@ if isSavedState():
 	loadState()
 
 
+print "total batches ", totalBatches
+
+
 while epochsDone < TOTAL_EPOCHS:
 	while batchesDone < totalBatches:
-		print "Batch ", batchesDone , " Epoch " , epochsDone
-		d_loss , g_loss = trainOnBatch(batchesDone)
-		print "Generator loss", g_loss, "Discriminator loss", d_loss, "Total:", g_loss[0] + d_loss[0]
 
 		if batchesDone%10 == 0:
 			saveState()
 
+		print "Batch ", batchesDone , " Epoch " , epochsDone
+		d_loss , g_loss = trainOnBatch(batchesDone)
+		print "Generator loss", g_loss, "Discriminator loss", d_loss
+
 		batchesDone += 1
+		
+	batchesDone = 0 
 	epochsDone += 1
 
 print "+++++++++++++++++++++++++"
