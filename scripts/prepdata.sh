37f7c1f2bc2f0dc07aad3dd242d5c3bef2d3c34f
Divam Gupta <divamgupta@gmail.com>
2016-05-02 15:13:21 +0530

    mini fix in prepdata.sh

--- scripts/prepdata.sh
+++ scripts/prepdata.sh
@@ -1,7 +1,7 @@
 mkdir ../data
 mkdir ../data/weights
 
-cd mkdir ../data
+cd ../data
 
 wget  -q  "https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AADIKlz8PR9zr6Y20qbkunrba/Img/img_align_celeba.zip?dl=1&pv=1" -O  celeb.zip
 unzip celeb.zip
